4c53a0fd90e06226eb869fc3e61b6799cf6c1b61
high-u <zen.crazyd@gmail.com>
2025-03-07 11:25:26 +0900

    Select LLM. Added system-prompt.

--- src/main.js
+++ src/main.js
@@ -9,112 +9,206 @@ import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
 import hljs from "highlight.js";
 import "highlight.js/styles/github-dark.css";
+import { jsonrepair } from "jsonrepair";
+import { systemPrompt } from "./const.js";
 
-const { button, div, input, sup, textarea, pre, ul, li, select, option, p } =
-  van.tags;
+(async () => {
+  const { button, div, input, sup, textarea, pre, ul, li, select, option, p } =
+    van.tags;
 
-let chatMessages = [];
+  let chatMessages = [];
 
-const markedWithHighlight = new Marked(
-  markedHighlight({
-    langPrefix: "hljs language-",
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : "plaintext";
-      return hljs.highlight(code, { language }).value;
-    },
-  }),
-);
+  const markedWithHighlight = new Marked(
+    markedHighlight({
+      langPrefix: "hljs language-",
+      highlight(code, lang) {
+        const language = hljs.getLanguage(lang) ? lang : "plaintext";
+        return hljs.highlight(code, { language }).value;
+      },
+    }),
+  );
 
-const addMessage = (message, role) => {
-  // message = message.replace(/[\n]+/, "\n");
-  const style = ((role) => {
-    if (role === "user") {
-      return "bg-base-300";
-    } else {
-      return "bg-base-100";
+  const addMessage = (message, role) => {
+    // message = message.replace(/[\n]+/, "\n");
+    const style = ((role) => {
+      if (role === "user") {
+        return "bg-base-300";
+      } else {
+        return "bg-base-100";
+      }
+    })(role);
+
+    function htmlToElement(html) {
+      var template = document.createElement("template");
+      html = html.trim();
+      template.innerHTML = html
+        .replace(/[\n\s]+(<p>|<li>|<ul>|<ol>)/g, "$1")
+        .replace(/(<\/p>|<\/li>|<\/ul>|<\/ol>)[\n\s]+/g, "$1");
+      return template.content;
     }
-  })(role);
 
-  function htmlToElement(html) {
-    var template = document.createElement("template");
-    html = html.trim();
-    template.innerHTML = html
-      .replace(/[\n\s]+(<p>|<li>|<ul>|<ol>)/g, "$1")
-      .replace(/(<\/p>|<\/li>|<\/ul>|<\/ol>)[\n\s]+/g, "$1");
-    return template.content;
-  }
+    const html = markedWithHighlight.parse(message);
+    // const htmlMinify = html.replace(/>[\n\s]+/g, ">").replace(/[\n\s]+</, "<");
+    console.log(html);
 
-  const html = markedWithHighlight.parse(message);
-  // const htmlMinify = html.replace(/>[\n\s]+/g, ">").replace(/[\n\s]+</, "<");
-  console.log(html);
+    const h = htmlToElement(html, "text/html");
+    const a = div(
+      { class: `card w-96 shadow-sm mb-4 w-full ${style}` },
+      div(
+        { class: "card-body" },
+        div({ class: "whitespace-pre-wrap break-words markdown-element" }, h),
+      ),
+    );
+    htmls.val = [a, ...htmls.val];
+  };
 
-  const h = htmlToElement(html, "text/html");
-  const a = div(
-    { class: `card w-96 shadow-sm mb-4 w-full ${style}` },
-    div(
-      { class: "card-body" },
-      div({ class: "whitespace-pre-wrap break-words markdown-element" }, h),
+  const send = async () => {
+    const llmName = selectedLlm.val;
+    const p = prompt.val;
+    addMessage(p, "user");
+    prompt.val = "";
+    chatMessages = [...chatMessages, { role: "user", content: p }];
+
+    const response = await ollama.chat({
+      model: llmName,
+      messages: chatMessages,
+      stream: true,
+    });
+
+    for await (const part of response) {
+      counter.val += part.message.content;
+      console.log(part.message);
+      replyArea.scrollTop = replyArea.scrollHeight;
+    }
+
+    const assistantReply = counter.val;
+    const hoge = assistantReply.replace(/(```json\n|\n```)/g, "");
+    console.log({ assistantReply });
+    console.log(JSON.stringify(hoge, null, "  "));
+
+    chatMessages = [
+      ...chatMessages,
+      { role: "assistant", content: counter.val },
+    ];
+
+    // LLM から不完全な JSON が返ることがあるからリペア。
+    // それとストリーミングで受け取ったjsonを読み込む場合などに利用できそう。
+    const json = JSON.parse(jsonrepair(hoge));
+    let markdown = "";
+    for (const e of json.files) {
+      markdown += `## ${e.filename}
+
+\`\`\`${e.language}
+${e.content}
+\`\`\`
+
+${e.explanation}
+`;
+    }
+    markdown += `\n\n${json.explanation}`;
+
+    addMessage(markdown, "assistant");
+    counter.val = "";
+  };
+
+  const counter = van.state("");
+  const prompt = van.state(
+    "タスク管理Webアプリケーションを HTML、JavaScript、CSS の 3 ファイルで作成してください。",
+  );
+
+  const storeLlm = localStorage.getItem("llm");
+  const selectedLlm = van.state(storeLlm);
+
+  const llms = await ollama.list();
+  console.log(llms);
+
+  const modelList = div(
+    { class: "mb-2" },
+    select(
+      {
+        class: "select",
+        oninput: (e) => {
+          selectedLlm.val = e.target.value;
+          localStorage.setItem("llm", e.target.value);
+        },
+      },
+      llms.models
+        .sort((a, b) => (a.model > b.model ? 1 : -1))
+        .map((e) => option({ selected: () => storeLlm === e.model }, e.model)),
     ),
   );
-  htmls.val = [a, ...htmls.val];
-};
 
-const send = async () => {
-  const p = prompt.val;
-  addMessage(p, "user");
-  prompt.val = "";
-  chatMessages = [...chatMessages, { role: "user", content: p }];
+  const dom5 = div(
+    { class: "mb-2" },
+    textarea({
+      value: prompt,
+      rows: 4,
+      cols: 100,
+      wrap: "soft",
+      class: "textarea w-full",
+      oninput: (e) => (prompt.val = e.target.value),
+    }),
+  );
 
-  const response = await ollama.chat({
-    model: "qwen2.5-coder-7b-instruct-q2_k:v1",
-    messages: chatMessages,
-    stream: true,
-  });
+  const sendBtn = div(
+    { class: "mb-4" },
+    button({ onclick: send, class: "btn btn-primary w-full" }, "Send"),
+  );
 
-  for await (const part of response) {
-    counter.val += part.message.content;
-    replyArea.scrollTop = replyArea.scrollHeight;
-  }
-  chatMessages = [...chatMessages, { role: "assistant", content: counter.val }];
-  addMessage(counter.val, "assistant");
-  counter.val = "";
-};
-
-const counter = van.state("");
-const prompt = van.state("HTML5 の雛形の作成と、その説明をお願いします。");
-
-const dom5 = div(
-  { class: "mb-2" },
-  textarea({
-    value: prompt,
-    rows: 4,
+  const replyArea = textarea({
+    value: counter,
+    rows: 8,
     cols: 100,
     wrap: "soft",
-    class: "textarea textarea-primary w-full",
-    oninput: (e) => (prompt.val = e.target.value),
-  }),
-);
+    class: "textarea w-full ",
+  });
+  const dom1 = div({ class: "mb-4" }, replyArea);
+  const htmls = van.state([]);
+  let m = div({ class: "" }, () => div({ class: "" }, htmls.val));
+  const form = div(
+    { class: "pt-8 flex items-center justify-center" },
+    div({ class: "w-[800px]" }, modelList, dom5, sendBtn, dom1, m),
+  );
 
-const sendBtn = div(
-  { class: "mb-4" },
-  button({ onclick: send, class: "btn btn-primary w-full" }, "Send"),
-);
+  document.body.classList.add("bg-base-200", "min-h-screen");
 
-const replyArea = textarea({
-  value: counter,
-  rows: 8,
-  cols: 100,
-  wrap: "soft",
-  class: "textarea textarea-primary w-full ",
-});
-const dom1 = div({ class: "mb-4" }, replyArea);
-const htmls = van.state([]);
-let m = div({ class: "" }, () => div({ class: "" }, htmls.val));
-const form = div(
-  { class: "pt-8 flex items-center justify-center" },
-  div({ class: "w-[800px]" }, dom5, sendBtn, dom1, m),
-);
+  van.add(document.body, form);
 
-document.body.classList.add("bg-base-200", "min-h-screen");
+  chatMessages = [{ role: "system", content: systemPrompt }];
+})();
 
-van.add(document.body, form);
+// https://regex101.com/
+
+/*
+=== FILE: ([^ =]+) ===[\n\s]*```(?:\w+)?\n([\s\S]*?)\n```
+*/
+
+/*
+=== FILE: index.html ===
+
+```html
+<html>
+    <body>
+        あいうえお
+    </body>
+</html>
+```
+
+=== FILE: main.js ===
+
+```js
+const hoge = "hoge";
+if (hoge === "hoge") {
+  console.log("hoge");
+}
+```
+
+=== FILE: style.css ===
+
+```css
+body {
+    background-color: white;
+    padding: 8rem;
+}
+```
+*/
